bf9dd4eab17da45741ea2b4036c99e8dc3db4c0a
lambeboluwatife <boluwatifelambe@gmail.com>
2024-10-08 15:11:47 +0100

    user login

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,7 +2,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
 
-// import User from "../models/User";
+const User = require("../models/User");
 
 passport.use(
   new LocalStrategy(
